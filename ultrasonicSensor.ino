49c0b0342668079e582b88d5965837a4496dbd61
Gabriel Correa <gabcralves@gmail.com>
2018-06-20 15:36:36 -0300

    Update ultrasonicSensor.ino

--- ultrasonicSensor.ino
+++ ultrasonicSensor.ino
@@ -3,8 +3,35 @@
  */
  
 #define trigPin 9
-#define trigPin 10
+#define echoPin 10
+
+int distance = 0;
 
 void setup() {
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    
+    Serial.begin(9600);
+}
 
+int distanceCalc(int duration) {
+    return duration*0.034/2;
+}
+
+void loop() {
+    //Clearing the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    distance = distanceCalc(pulseIn(echoPin, HIGH));
+    
+    if(distance <= 10) {
+        // ...
+        // Detour and continue
+    }
+    
 }
